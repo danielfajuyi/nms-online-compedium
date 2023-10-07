7c7d4a0b669a9694477c127fb96be6e4cd428d2d
abdullah <sirabdull70@gmail.com>
2023-10-07 16:25:56 -0700

    first commit to this branch

--- src/Pages/LandingPage/Layout/HeroSection/Hero/hero.jsx
+++ src/Pages/LandingPage/Layout/HeroSection/Hero/hero.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./hero.scss";
+import "./hero.css";
 
 const Hero = () => {
   return (
     <>
       <header className="hero-container">
-        <section className="hero-section container"></section>
+        <section className="hero-section container">
+          <span>NIGERIAN</span><span>MILITARY</span><span>SCHOOL</span>
+          <span>ZARIA</span><br /><span><kbd>online compedium</kbd></span>
+        </section>
       </header>
     </>
   );

3e0ee598aa40a741bfffc5f010c499b3e691ac74
abdullah <sirabdull70@gmail.com>
2023-10-21 01:49:10 -0700

    merged daniel

---
+++ backend/server/phppages/dbconnect.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+##connection variables
+$server = "localhost";
+$user = "sirabdull";
+## password is confidential
+$password = "dashnov4";
+$database = "candidates";
+$suscess= '<h1>'.'connection sucessfull'.'<h1>';
+$notestablished= '<h1>'.'connection UNsucessfull'.'<h1>';
+##establishing connection
+
+$conn = mysqli_connect($server,$user,$password,$database);
+if(!$conn){
+    echo $notestablished;
+}
---
+++ backend/server/phppages/session.php
@@ -0,0 +1,8 @@
+<?php
+#creating sessions for candidates
+session_start();
+$_SESSION['username'] = $user;
+$_SESSION['email']= $email;
+$_SESSION['image'] = $image
+
+?>

d59e6973378a310d8dde8d0627f9368546d63e6b
sirabdull <144572998+sirabdull@users.noreply.github.com>
2023-12-09 15:33:36 -0800

    Delete backend directory

--- backend/server/phppages/dashboard/includes/signout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_destroy();
-    session_abort();
-    session_destroy();
-
-    header('location:http://localhost:3000');
-
-?>
--- backend/server/phppages/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-#creating sessions for candidates
-session_start();
-
-
-
-$_SESSION['email']= $_POST['email'];
-
-?>
